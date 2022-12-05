3f7f6ef32ef031e7040e10f5b495711d5ac72b4b
LAPTOP-QS1LABDN\quan6 <qhuynh@vhslab.com>
2022-12-05 10:51:34 -0500

    test

--- ponder.config.js
+++ ponder.config.js
@@ -9,15 +9,15 @@ const ponderConfig = {
     kind: "sqlite",
   },
   networks: [
-    { name: "goerli", chainId: 5, rpcUrl: process.env.PONDER_RPC_URL_1 },
+    { name: process.env.NETWORK, chainId: parseInt(process.env.CHAINID), rpcUrl: process.env.PONDER_RPC_URL_1 },
   ],
   sources: [
     {
       name: "AnomuraEquipment",
-      network: "goerli",
-      address: "0xE394Ac77f89FbFAF464FC5796f90C2E192c2f2de",
+      network: process.env.NETWORK,
+      address: process.env.ANOMURA_EQUIPMENT_ADDRESS,
       abi: "./abis/AnomuraEquipment.json",
-      startBlock: 7990478,
+      startBlock: parseInt(process.env.ANOMURA_EQUIPMENT_START_BLOCK),
     },
   ],
 };
